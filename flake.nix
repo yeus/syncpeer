b78a03ce8c736a0b283be8721e709c8fc6aca9f7
yeus <Thomas.Meschede@web.de>
2026-03-22 15:38:53 -0700

    add some project bureaucracy

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "syncpeer - lightweight Syncthing BEP CLI client";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        nodejs = pkgs.nodejs_22;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+            jq
+            openssl
+            protobuf
+            nixpkgs-fmt
+          ];
+
+          shellHook = ''
+            echo "syncpeer dev shell"
+            echo "node: $(node --version)"
+            echo "npm:  $(npm --version)"
+          '';
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
+}
